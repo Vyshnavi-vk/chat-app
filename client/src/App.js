ca6c4eedd309a5d2a49be5d2cbe3ec99370a71cf
Vyshnavi-vk <kuntumallavyshnavi@gmail.com>
2023-04-09 13:23:51 +0530

    commit

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+// import Join from './components/Join/Join';
+// import Chat from './components/Chat/Chat';
+import Join from './components/Join/Join';
+import Chat from './components/Chat/Chat';
+
+const App = () => {
+
+
+  return (
+    <>
+        <BrowserRouter>
+            <Routes>
+                 <Route  path="/" exact element={<Join/>}/>
+                 <Route  path="/chat" exact element={<Chat/>}/>
+            </Routes>
+        </BrowserRouter>
+        
+    </>
+  )
+  
+};
+
+export default App;
